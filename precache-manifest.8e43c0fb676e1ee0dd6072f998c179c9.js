90e01d12beb15cbe520063880d5f31b1b762acbd
Flor <florxgomez@gmail.com>
2019-11-22 02:43:24 -0300

    Updates

---
+++ precache-manifest.8e43c0fb676e1ee0dd6072f998c179c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcdec144a7a891a0484bda1cbf2985db",
+    "url": "/lyrics-finder/index.html"
+  },
+  {
+    "revision": "30366a66811e3bbfeb10",
+    "url": "/lyrics-finder/static/css/main.1cda5295.chunk.css"
+  },
+  {
+    "revision": "8f0977617c6e69830542",
+    "url": "/lyrics-finder/static/js/2.24318567.chunk.js"
+  },
+  {
+    "revision": "30366a66811e3bbfeb10",
+    "url": "/lyrics-finder/static/js/main.10b3e8c8.chunk.js"
+  },
+  {
+    "revision": "48d2460550283183996a",
+    "url": "/lyrics-finder/static/js/runtime-main.2996c753.js"
+  }
+]);
