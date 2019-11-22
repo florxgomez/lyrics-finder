529725cb5e9a3d03296315fb03fe0ce4cf6c816c
Flor <florxgomez@gmail.com>
2019-11-22 02:54:54 -0300

    fix base url

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Index} />
-              <Route exact path="lyrics/track/:id" component={Lyrics} />
+              <Route exact path="/lyrics/track/:id" component={Lyrics} />
             </Switch>
           </div>
         </Fragment>
